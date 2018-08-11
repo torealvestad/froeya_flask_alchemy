212ea0b16902086d489a88a2b74e91d274dce29b
Tore Alvestad <tore.alvestad@gmail.com>
2018-08-11 08:48:48 +0200

    "first commit"

--- application.py
+++ application.py
@@ -2,8 +2,13 @@ import datetime
 
 from flask import Flask, render_template, request, session
 from flask_session import Session
+from models import *
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
